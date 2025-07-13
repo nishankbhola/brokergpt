bc3c037feb8db041ea10996f31a70d2c0cf28230
Nishank Bhola <40416237+nishankbhola@users.noreply.github.com>
2025-07-13 03:36:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-#import pysqlite3 as sqlite3
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3 as sqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import shutil
 import json
