002cf2271781d980d35632465064528b2b44e18a
Nishank Bhola <40416237+nishankbhola@users.noreply.github.com>
2025-07-13 03:36:53 -0400

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,3 +1,6 @@
+import pysqlite3 as sqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
