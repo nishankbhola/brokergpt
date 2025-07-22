d3d445911d28f9b19fb7b3947651e7a42d925c7f
Nishank Bhola <40416237+nishankbhola@users.noreply.github.com>
2025-07-22 09:10:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -941,7 +941,7 @@ else:
 st.markdown("---")
 st.markdown(
     "<div style='text-align: center; color: gray;'>"
-    "🤖 BIBLIO | Powered by AI | Version 20.1.1 | 2025"
+    "🤖 BIBLIO | Powered by AI | Version 21.1.1 | 2025"
     "</div>", 
     unsafe_allow_html=True
 )
